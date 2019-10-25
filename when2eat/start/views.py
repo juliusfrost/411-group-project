097e736e7f827e5f8da88db3c9a61b49a4d4b112
Julius Frost <juliustfrost@gmail.com>
2019-10-25 00:41:00 -0400

    Add start page draft

--- when2eat/start/views.py
+++ when2eat/start/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def start(request):
-    return HttpResponse("You are at the start page.")
+    return render(request, 'start/index.html')
