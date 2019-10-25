9960da3d1ec9b5798cdd4fea589885691a2a3b00
Julius Frost <juliustfrost@gmail.com>
2019-10-25 00:08:39 -0400

    Added start page

--- when2eat/when2eat/settings.py
+++ when2eat/when2eat/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'start.apps.StartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
