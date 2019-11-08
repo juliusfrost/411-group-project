8ac756979a236fb35d844059494bbfbb9595d108
Julius Frost <juliustfrost@gmail.com>
2019-11-08 10:57:50 -0500

    Add session id

--- when2eat/session/urls.py
+++ when2eat/session/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('join/', views.join, name='join'),
+    path('join/<int:session_id>', views.join_session, name='join'),
 ]
