6c5919aad035f72698043fd7e6b68bfaf20a4965
Chris Gil Martins <cgm343@bu.edu>
2019-11-14 13:08:15 -0500

    static references

--- when2eat/start/views.py
+++ when2eat/start/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def start(request):
-    return render(request, 'start/index.html')
+    context = {}
+    if request.user.is_authenticated:
+        context["authtest"] = "You logged in!"
+    return render(request, 'start/index.html', context)
